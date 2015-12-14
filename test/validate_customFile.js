29740880ab703263b8dc1d9725e6694029d64a88
angelozerr <angelo.zerr@gmail.com>
2015-12-14 12:11:32 +0100

    Use fs.watch to reload .jshintrc when it changes. Don't crash when .jshintrc is not JSON valid.

--- test/validate_customFile.js
+++ test/validate_customFile.js
@@ -1,6 +1,42 @@
 var util = require("./util");
 
-exports['test undef with config file'] = function() {
+exports['test unkkown .jshintrc'] = function() {
+  
+  // Unknown .jshintrc file
+
+  util.assertLint("var a = angular, b = XXX;", {
+    messages : [{
+      "message" : "Cannot find JSHint config files ['XXX/.jshintrc', 'C://Users//azerr//git//tern-jshint//XXX/.jshintrc']",
+      "severity" : "error",
+      "from" : 0,
+      "to" : 1,            
+      "file": "test1.js"}
+    ]
+  }, null, {
+    "configFile": "XXX/.jshintrc"
+  });
+  
+}
+
+exports['test bad JSON .jshintrc'] = function() {
+  
+  // bad JSON .jshintrc file
+
+  util.assertLint("var a = angular, b = XXX;", {
+    messages : [{
+      "message" : "Error: Can't parse config file: test/.jshintrc_bad\nError:SyntaxError: Unexpected token :",
+      "severity" : "error",
+      "from" : 0,
+      "to" : 1,            
+      "file": "test1.js"}
+    ]
+  }, null, {
+    "configFile": "test/.jshintrc_bad"
+  });
+  
+}
+
+exports['test undef with .jshintrc'] = function() {
   
   // Undef 
 
